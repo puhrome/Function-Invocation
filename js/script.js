21a151e2b0f6b81431b17ae42681ee050d1eba47
puhrome <phirom.yim@fullsail.edu>
2014-04-29 17:53:01 -0400

    Invocation of a Function
    Calling or Invoking a Function in WebStorm

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/**
+ * Function Invocation Created by puhrome on 4/29/14.
+ */
+//function only written once
+function outptMsg() {
+    console.log("Hello World");//inside curly braces, block of code
+}
+//function definition
+function calcArea(){
+    var width = 20;//code inside braces
+    var height = 30;//variable for height
+    var area = width * height;//variable for are
+    console.log(area);//print code
+}
+//function needs to be invoked or called to run
+calcArea();//actual invocation of the function
+calcArea();//function execution
+calcArea();///making the function reusable and repeatable
